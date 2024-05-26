f02c9ad0676d1ba57ed460fd41097ab8c972a6da
ganga0116 <130018684+ganga0116@users.noreply.github.com>
2024-05-26 20:08:26 +0930

    Update EventOperations.java
    listevent is added .

--- OnlineBookingSystem/src/EventManagementSystem/EventOperations.java
+++ OnlineBookingSystem/src/EventManagementSystem/EventOperations.java
@@ -4,6 +4,7 @@ public interface EventOperations {
 	void addEvent(Event event);
     void updateEvent(int eventId, String newName, java.util.Date newDate, String newVenue);
     void deleteEvent(int eventId);
+    void listEvents();
     void viewEventDetails(int eventId);
     void generateEventReport();
 }
